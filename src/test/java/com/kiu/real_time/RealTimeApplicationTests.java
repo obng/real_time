1e0d0609b01822834b62238468bf5b07e25616b7
bong <p1-02@naver.com>
2025-04-06 22:45:32 +0900

    feat: 프로젝트 파일 추가

---
+++ src/test/java/com/kiu/real_time/RealTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiu.real_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
