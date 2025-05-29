2d8b749250bef68272e851fc8a20cda8828016c7
hoon2001 <bide851@naver.com>
2025-05-30 02:17:34 +0900

    fix; 별점(점수) 등록 사이트 수정

--- src/main/frontend/src/layouts/evaluation/EvaluationForm/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as EvaluationForm } from './EvaluationForm';
-export { default as WorkerEvaluate } from './workerEvaluate';
-export { default as OwnerEvaluate } from './ownerEvaluate';
